497bb0961e6cababc1139e566affba4fcbff55be
ShreeSamal <shree.samal1502@gmail.com>
2023-10-19 18:32:57 +0530

    added government logics

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def login_required(func):
     @wraps(func)
     def decorated_view(*args, **kwargs):
         if 'email' not in session:
-            return redirect('/login')
+            return redirect('/')
         return func(*args, **kwargs)
     return decorated_view
 
@@ -49,7 +49,7 @@ def time_difference(timestamp):
 @login_required
 def society_homepage(society_id):
     complaints_ref = db.collection('complaints')
-    documents = complaints_ref.stream()
+    documents = complaints_ref.where("society_id","==",society_id).stream()
     complaints = []
     for doc in documents:
         curr = doc.to_dict()
@@ -57,7 +57,7 @@ def society_homepage(society_id):
         complaints.append({'complaint':curr['complaint'],'diff':diff})
     return render_template('index.html',title='Home',society_id=society_id,complaints=complaints[-5:])
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def login():
     if request.method == "POST":
         email = request.form['email']
@@ -68,10 +68,15 @@ def login():
         for doc in documents:
             user = doc.to_dict()
         if len(user)>=2 and user['password'] == password:
+            session.clear()
             session['email'] = user['email']
+            print("logged in")
             if user['role'] == 'secretary':
                 session['society_id'] = user['society_id']
-            return redirect('/society/'+str(user['society_id']))
+                return redirect('/society/'+str(user['society_id']))
+            else:
+                return redirect('/government')
+            
         else:
             return render_template('pages-login.html',message="Login failed. Please check your credentials.")
     return render_template('pages-login.html')
@@ -79,7 +84,10 @@ def login():
 @app.route('/complaints')
 def complaints():
     complaints_ref = db.collection('complaints')
-    documents = complaints_ref.stream()
+    if 'society_id' in session:
+        documents = complaints_ref.where('society_id', '==', session['society_id']).stream()
+    else:
+        documents = complaints_ref.stream()
     complaints = []
     for doc in documents:
         curr = doc.to_dict()
@@ -396,6 +404,60 @@ def calculate_average_weekly_data(society_id, year, month, value_type):
     
 #     return jsonify(monthly_data)
 
+# Get year data for district
+@app.route('/api/year/district/<district>/<year>/<value_type>')
+def calculate_average_monthly_district_data(district, year, value_type):
+    year = int(year)
+    keys_to_average = [value_type]
+
+    society_ref = db.collection('society')
+    documents = society_ref.where('district', '==', district).stream()
+
+    data = []
+    for doc in documents:
+        data.append(doc.to_dict())
+
+    monthly_data = {}
+
+    for month in range(1, 13):
+        start_date = datetime(year, month, 1)
+        if month == 12:
+            end_date = datetime(year + 1, 1, 1)
+        else:
+            end_date = datetime(year, month + 1, 1)
+
+        # Filter data for the specified month and year
+        filtered_data = [entry for entry in data if start_date <= datetime.fromtimestamp(entry["timestamp"]) < end_date]
+
+        if filtered_data:
+            key_averages = {}
+            for key_to_average in keys_to_average:
+                key_averages[key_to_average] = round(sum(entry[key_to_average] for entry in filtered_data) / len(filtered_data), 2)
+
+            key_averages["timestamp"] = filtered_data[0]["timestamp"]
+            monthly_label = f'mnth{month}'  # Create month labels mnth1, mnth2, ...
+            monthly_data[monthly_label] = key_averages
+
+
+    # Sort the month labels and values based on month numbers
+    sorted_months_and_values = sorted(
+        [(int(month_label[4:]), monthly_data[month_label][value_type]) for month_label in monthly_data],
+        key=lambda x: x[0]
+    )
+
+    # Check if the list is empty and return empty arrays
+    if not sorted_months_and_values:
+        return jsonify({'months': [], 'values': []})
+
+    sorted_month_labels, sorted_values = zip(*sorted_months_and_values)
+
+    return jsonify({
+        'months': sorted_month_labels,
+        'values': sorted_values
+    })
+
+
+# Get year data for society
 @app.route('/api/year/<society_id>/<year>/<value_type>')
 def calculate_average_monthly_data(society_id, year, value_type):
     society_id = int(society_id)
@@ -448,6 +510,150 @@ def calculate_average_monthly_data(society_id, year, value_type):
         'values': sorted_values
     })
 
+
+# Get year data for locality
+@app.route('/api/year/locality/<locality>/<year>/<value_type>')
+def calculate_average_monthly_locality_data(locality, year, value_type):
+    year = int(year)
+    keys_to_average = [value_type]
+
+    society_ref = db.collection('society')
+    documents = society_ref.where('locality', '==', locality).stream()
+
+    data = []
+    for doc in documents:
+        data.append(doc.to_dict())
+
+    monthly_data = {}
+
+    for month in range(1, 13):
+        start_date = datetime(year, month, 1)
+        if month == 12:
+            end_date = datetime(year + 1, 1, 1)
+        else:
+            end_date = datetime(year, month + 1, 1)
+
+        # Filter data for the specified month and year
+        filtered_data = [entry for entry in data if start_date <= datetime.fromtimestamp(entry["timestamp"]) < end_date]
+
+        if filtered_data:
+            key_averages = {}
+            for key_to_average in keys_to_average:
+                key_averages[key_to_average] = round(sum(entry[key_to_average] for entry in filtered_data) / len(filtered_data), 2)
+
+            key_averages["timestamp"] = filtered_data[0]["timestamp"]
+            monthly_label = f'mnth{month}'  # Create month labels mnth1, mnth2, ...
+            monthly_data[monthly_label] = key_averages
+
+
+    # Sort the month labels and values based on month numbers
+    sorted_months_and_values = sorted(
+        [(int(month_label[4:]), monthly_data[month_label][value_type]) for month_label in monthly_data],
+        key=lambda x: x[0]
+    )
+
+    # Check if the list is empty and return empty arrays
+    if not sorted_months_and_values:
+        return jsonify({'months': [], 'values': []})
+
+    sorted_month_labels, sorted_values = zip(*sorted_months_and_values)
+
+    return jsonify({
+        'months': sorted_month_labels,
+        'values': sorted_values
+    })
+
+# Get district month data
+@app.route('/api/month/locality/<locality>/<year>/<month>/<value_type>')
+def calculate_average_locality_weekly_data(locality, year, month, value_type):
+    year = int(year)
+    month = int(month)
+
+    society_ref = db.collection('society')
+    documents = society_ref.where('locality', '==', locality).stream()
+
+    data = []
+    for doc in documents:
+        data.append(doc.to_dict())
+    
+    weekly_data = {}
+    start_date = datetime(year, month, 1)
+    if month == 12:
+        end_date = datetime(year + 1, 1, 1)
+    else:
+        end_date = datetime(year, month + 1, 1)
+
+    # Iterate through the data, filter it for the specified month and year, and group it by week
+    for entry in data:
+        entry_date = datetime.fromtimestamp(entry["timestamp"])
+        
+        if start_date <= entry_date < end_date:
+            week_number = (entry_date - start_date).days // 7 + 1
+            
+            if week_number not in weekly_data:
+                weekly_data[week_number] = []
+            
+            weekly_data[week_number].append(entry)
+
+    averages = {}
+    keys_to_average = [value_type]  # Use the specified value_type
+    week_labels = []  # Store week labels
+
+    for key, values in weekly_data.items():
+        key_averages = {}
+        for entry in values:
+            for key_to_average in keys_to_average:
+                if key_to_average not in key_averages:
+                    key_averages[key_to_average] = 0
+                key_averages[key_to_average] += entry[key_to_average]
+
+        if len(values) > 0:
+            for key_to_average in keys_to_average:
+                key_averages[key_to_average] /= len(values)
+                key_averages[key_to_average] = round(key_averages[key_to_average], 2)
+
+        key_averages["timestamp"] = values[0]["timestamp"]
+        averages[key] = key_averages
+        week_labels.append(f'wk{key}')  # Create week labels
+        
+    # Check if averages is empty, and return the empty response if there's no data
+    if not averages:
+        return jsonify({'weeks': [], 'values': []})
+
+    # Sort week labels and values based on week numbers
+    sorted_weeks_and_values = sorted(zip(week_labels, [entry[value_type] for entry in averages.values()]), key=lambda x: int(x[0][2:]))
+    sorted_week_labels, sorted_values = zip(*sorted_weeks_and_values)
+
+    response_data = {
+        'weeks': sorted_week_labels,
+        'values': sorted_values
+    }
+
+    return jsonify(response_data)
+
+
+# Open new district page 
+@app.route('/district/<district>')
+def district_homepage(district):
+    return render_template('district.html',title='District',district=district)
+
+# Open new locality page
+@app.route('/locality/<locality>')
+def locality_homepage(locality):
+    return render_template('locality.html',title='Locality',locality=locality)
+
+# Government Route
+@app.route('/government')
+def government_homepage():
+    society_ref = db.collection('society')
+    documents = society_ref.stream()
+    unique_localities = set()
+    unique_districts = set()
+    for doc in documents:
+        curr = doc.to_dict()
+        unique_localities.add(curr['locality'])
+        unique_districts.add(curr['district'])
+    return render_template('government.html',title='Government',localities=unique_localities,districts=unique_districts)
 
 # Enable debug mode
 app.debug = True
